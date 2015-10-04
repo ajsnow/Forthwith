312043de1109019421030814da966594a4c0a3d9
Andrew Snow <andrew.j.snow@gmail.com>
2015-10-03 23:04:58 -0500

    Initial Commit

---
+++ SwiftlyForth/SwiftlyForth.h
@@ -0,0 +1,19 @@
+//
+//  SwiftlyForth.h
+//  SwiftlyForth
+//
+//  Created by Andrew Snow on 10/3/15.
+//  Copyright © 2015 Andrew Snow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftlyForth.
+FOUNDATION_EXPORT double SwiftlyForthVersionNumber;
+
+//! Project version string for SwiftlyForth.
+FOUNDATION_EXPORT const unsigned char SwiftlyForthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftlyForth/PublicHeader.h>
+
+
