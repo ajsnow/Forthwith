2a1bf4a94a10b63edda18e6c9c1c8d16ea1655d9
Andrew Snow <andrew.j.snow@gmail.com>
2015-10-11 20:04:37 -0500

    Rename Forthwith.h – missed it in the rename

---
+++ Source/Forthwith.h
@@ -0,0 +1,19 @@
+//
+//  Forthwith.h
+//  Forthwith
+//
+//  Created by Andrew Snow on 10/3/15.
+//  Copyright © 2015 Andrew Snow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Forthwith.
+FOUNDATION_EXPORT double ForthwithVersionNumber;
+
+//! Project version string for Forthwith.
+FOUNDATION_EXPORT const unsigned char ForthwithVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Forthwith/PublicHeader.h>
+
+
